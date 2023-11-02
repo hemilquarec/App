29ddcaed2ff00282d1a4d212b490a1114dd3ae99
hemilquarec <hemil.quarec@gmail.com>
2023-11-02 11:19:25 +0530

    bh

--- lib/main.dart
+++ lib/main.dart
@@ -24,27 +24,62 @@ class _WebViewExampleState extends State<WebViewExample> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        // title: Text('Gujarat Vandan'),
-      ),
-      body: WebView(
-        initialUrl: 'https://gujaratvandan.com/', // Change to the URL you want to display
-        javascriptMode: JavascriptMode.unrestricted,
-        onWebResourceError: (WebResourceError error) {
-          print("Error occurred: ${error.description}");
-          // Display a user-friendly error message or take appropriate action here.
-        },
-        onWebViewCreated: (WebViewController controller) {
-          _controller = controller;
-        },
-        navigationDelegate: (NavigationRequest request) {
-          if (request.url == 'https://gujaratvandan.com/') {
-            canNavigateBack = false;
-          } else {
-            canNavigateBack = true;
+      body: WillPopScope(
+        onWillPop: () async {
+          if (canNavigateBack) {
+            if (await _controller.canGoBack()) {
+              _controller.goBack();
+              return false; // Prevent the app from exiting
+            }
           }
-          return NavigationDecision.navigate;
+
+          // If not on the root page, allow back navigation in WebView
+          if (canNavigateBack) {
+            return true;
+          } else {
+            // Show a confirmation dialog to exit the app
+            return await showDialog(
+              context: context,
+              builder: (context) => AlertDialog(
+                title: Text('Exit App'),
+                content: Text('Are you sure you want to exit the app?'),
+                actions: <Widget>[
+                  TextButton(
+                    onPressed: () {
+                      Navigator.of(context).pop(false); // Cancel the exit
+                    },
+                    child: Text('No'),
+                  ),
+                  TextButton(
+                    onPressed: () {
+                      Navigator.of(context).pop(true); // Confirm the exit
+                    },
+                    child: Text('Yes'),
+                  ),
+                ],
+              ),
+            );
+          }
         },
+        child: WebView(
+          initialUrl: 'https://gujaratvandan.com/',
+          javascriptMode: JavascriptMode.unrestricted,
+          onWebResourceError: (WebResourceError error) {
+            print("Error occurred: ${error.description}");
+            // Display a user-friendly error message or take appropriate action here.
+          },
+          onWebViewCreated: (WebViewController controller) {
+            _controller = controller;
+          },
+          navigationDelegate: (NavigationRequest request) {
+            if (request.url == 'https://gujaratvandan.com/') {
+              canNavigateBack = false;
+            } else {
+              canNavigateBack = true;
+            }
+            return NavigationDecision.navigate;
+          },
+        ),
       ),
     );
   }
@@ -53,16 +88,5 @@ class _WebViewExampleState extends State<WebViewExample> {
   void initState() {
     super.initState();
     canNavigateBack = false;
-  }
-
-  @override
-  Future<bool> didPopRoute() async {
-    if (canNavigateBack) {
-      if (await _controller.canGoBack()) {
-        _controller.goBack();
-        return true;
-      }
-    }
-    return false;
   }
 }
